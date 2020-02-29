87aa56830594e99e52f5c1a4eeb523033a724345
pglez82 <pglez82@gmail.com>
2020-02-29 09:03:27 +0100

    Updates

---
+++ precache-manifest.75db4adf0b1a73f59625f480d8fbaf20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a712ae6f1a7b8c7507e9e992b3c88adc",
+    "url": "/npm-tutorial/index.html"
+  },
+  {
+    "revision": "e840514e7f02cc44457e",
+    "url": "/npm-tutorial/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5f43afdda7ca321c3b92",
+    "url": "/npm-tutorial/static/js/2.28d221d8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/npm-tutorial/static/js/2.28d221d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e840514e7f02cc44457e",
+    "url": "/npm-tutorial/static/js/main.32da7d0a.chunk.js"
+  },
+  {
+    "revision": "4c27560695c76556b79b",
+    "url": "/npm-tutorial/static/js/runtime-main.ef9d4001.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/npm-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
